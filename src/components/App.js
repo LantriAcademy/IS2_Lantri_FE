ff05c9c837cc000521c26dc428ec96532d2160da
Raitozy <judgarciani@unal.edu.co>
2018-03-10 16:29:52 -0500

    MenuInicial

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/FundacionesLOGO.jpg';
 import '../styles/App.css';
+import NavigationBar from "../components/NavigationBar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <img src={logo} className="App-logo"/>  
+          <h1 className ="App-title">fUNdaciones</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <NavigationBar name = "Inicio"/>
+          <NavigationBar name = "Fundaciones"></NavigationBar>
+          <NavigationBar name = "Proposito"></NavigationBar>
+          <NavigationBar name = "Quienes Somos"></NavigationBar>
+          <NavigationBar name = "Contactenos"></NavigationBar>
+        </ul>
       </div>
     );
   }
