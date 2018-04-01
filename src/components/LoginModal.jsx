bb756d5dc6dd0ef083fa43556ec0570ea3e57d63
Raitozy <judgarciani@unal.edu.co>
2018-04-01 01:21:32 -0500

    Validaciones  Login y registro

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -2,9 +2,55 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Modal} from 'react-bootstrap';
 import "../styles/LoginModal.css";
+import { FormErrors } from "./FormErrors.js"
 
 class LoginModal extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      formErrors: {email: '', password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
+    }
+  }
 
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value},
+                  () => { this.validateField(name, value) });
+}
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+  
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' no es valido';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' debe tener almenos 6 caracteres';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
+  }
+  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+  
   render() {
     return (
       <div>
@@ -16,15 +62,27 @@ class LoginModal extends React.Component {
           </Modal.Header>
           <Modal.Body>
             <form className="login">
-              <div className="form-group">
+              <div className={"from-group"}>
                 <label>Correo Electronico</label>
-                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Correo Electronico" />
+                <input type="email" className="form-control" name="email" 
+                      aria-describedby="emailHelp" 
+                      placeholder="Correo Electronico"
+                      value={this.state.email}
+                      onChange={this.handleUserInput} />
               </div>
-              <div className="form-group">
+              <div className={"from-group"}>
+              <br/>
                 <label>Contraseña</label>
-                <input type="password" className="form-control" id="password" placeholder="Contraseña" />
+                <input type="password" className="form-control" name="password" 
+                      placeholder="Contraseña"                      
+                      value={this.state.password}
+                      onChange={this.handleUserInput} />
               </div>
-              <button type="submit" className="btn btn-success  btn-block ">Iniciar Sesion</button>
+              <div>
+              <br/>
+                <FormErrors formErrors={this.state.formErrors} />
+              </div>
+              <button type="submit" className="btn btn-success"  disabled={!this.state.formValid}>Iniciar Sesion</button>
               <p>¿No tienes cuenta?  <Link to="/signup" onClick={this.props.hide}>Registrarse</Link></p>
             </form>
           </Modal.Body>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 import '../styles/SignUp.css';
 import{FormControl, FormGroup, ControlLabel} from "react-bootstrap"
+import { FormErrors } from "./FormErrors.js"
 
 class SignUp extends Component {
-    render() {
+  
+  constructor (props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      formErrors: {email: '', password: ''},
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
+    }
+  }
+
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value},
+                  () => { this.validateField(name, value) });
+}
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+  
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' no es valido';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' debe tener almenos 6 caracteres';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
+  }
+  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }   
+  
+  
+  render() {
       return (
         <div className="caja" >
           <form className="login">
@@ -14,12 +63,19 @@ class SignUp extends Component {
             </FormGroup>
             <FormGroup>
               <ControlLabel>Correo Electronico</ControlLabel>
-              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Correo Electronico"/>
+              <input type="email" className="form-control" name="email" 
+                      aria-describedby="emailHelp" 
+                      placeholder="Correo Electronico"
+                      value={this.state.email}
+                      onChange={this.handleUserInput} />
             </FormGroup>
             <FormGroup>
               <ControlLabel>Contraseña</ControlLabel>
-              <input type="password" className="form-control" id="password" placeholder="Contraseña"/>
-            </FormGroup>
+              <input type="password" className="form-control" name="password" 
+                      placeholder="Contraseña"                      
+                      value={this.state.password}
+                      onChange={this.handleUserInput} />
+             </FormGroup>
             <FormGroup controlId="formControlsSelect">
               <ControlLabel>Tipo de usuario</ControlLabel>
               <FormControl componentClass="select" placeholder="select">
@@ -27,7 +83,11 @@ class SignUp extends Component {
                 <option value="fundacion">Fundacion</option>
               </FormControl>
             </FormGroup>
-              <button type="submit" className="btn btn-success">Registrarse</button>
+            <div>
+              <br/>
+                <FormErrors formErrors={this.state.formErrors} />
+            </div> 
+              <button type="submit" className="btn btn-success" disabled={!this.state.formValid}>Registrarse</button>
           </form>
         </div>
       );
