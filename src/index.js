0b30fe55836aa42749591e3028d7a186cb73975a
Nicolás Campuzano <ncampuzano@users.noreply.github.com>
2018-04-04 01:28:46 -0500

    Installing redux

--- src/index.js
+++ src/index.js
@@ -4,5 +4,36 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//import { fUNdationReducer } from './components/Redux/reducers';
+import { combineReducers } from 'redux'
+
+const User = (state={}, action) =>{
+    switch(action.type){
+        case "LOGIN":
+            state = {
+                "token" : action.token,
+                "id": action.id,
+                "foundationId": action.foundationId
+            } 
+        break;
+        case "LOGOFF":
+            state = {}
+        break;
+    }
+    return state;  
+}
+
+ 
+const fUNdationReducer = combineReducers({
+    User
+});
+const store = createStore(fUNdationReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
