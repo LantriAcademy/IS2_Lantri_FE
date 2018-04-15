7b27ef060ec2f8ec98a9181415e9e3212ec01e69
Raitozy <judgarciani@unal.edu.co>
2018-04-15 04:26:14 -0500

    a

--- src/components/PDF.jsx
+++ src/components/PDF.jsx
@@ -4,38 +4,11 @@ import PDFViewer from 'mgr-pdf-viewer-react'
 
 export default class PDF extends Component {
     render() {
-    return (<PDFViewer document={{
-        file: PDFSample,
-        //url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf'
-        //base64: ''
-    }} />);
+    return (
+        <PDFViewer document={{
+            //file: PDFSample
+            url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf'
+            //base64: ''
+        }} />);
   }
 }
-
-
-/*export default class PDF extends Component {
-    state = {
-      numPages: null,
-      pageNumber: 1,
-    }
-   
-    onDocumentLoad = ({ numPages }) => {
-      this.setState({ numPages });
-    }
-   
-    render() {
-      const { pageNumber, numPages } = this.state;
-   
-      return (
-        <div className="text-center">
-          <Document className="text-center"
-            file= {PDFSample}
-            onLoadSuccess={this.onDocumentLoad}
-          >
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <p>Page {pageNumber} of {numPages}</p>
-        </div>
-      );
-    }
-  }*/
