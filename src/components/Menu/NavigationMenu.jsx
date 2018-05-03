4da8fd7891fb51d1b69586ccf44beccc2e3ccfaa
Juan David Garcia <judgarciani@unal.edu.co>
2018-05-03 20:38:59 +0000

    boton oculto para Director

--- src/components/Menu/NavigationMenu.jsx
+++ src/components/Menu/NavigationMenu.jsx
@@ -57,7 +57,7 @@ class NavigationMenu extends Component {
                             <NavItem eventKey={3} componentClass={Link} href="/proposito" to="/proposito">Proposito</NavItem>
                             <NavItem eventKey={4} componentClass={Link} href="/quienessomos" to="/quienessomos">Quienes Somos</NavItem>
                             <NavItem eventKey={5} componentClass={Link} href="/contactenos" to="/contactenos">Contactenos</NavItem>
-                            {this.props.user.token !== undefined && this.props.user.token !== ""? (
+                            {this.props.user.token !== undefined && this.props.user.userType === false && this.props.user.token !== ""? (
                                 <NavItem eventKey={6} componentClass={Link} href="/perfil" to="/perfil">Perfil</NavItem>
                             ):(console.log(""))
                             }
